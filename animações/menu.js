a9ae8050a2996669d07250438eb3db11652a3411
dxsoftd <dxsoftd@gmail.com>
2023-03-05 20:41:33 +0000

    adicionei os eventos da escola na página intituição. :) :+1:

--- animações/menu.js
+++ animações/menu.js
@@ -3,26 +3,26 @@
 //VARIÁVEIS
 var icone = window.document.getElementById('iburger')
 
-        //FUNÇÃO PARA AJUDAR NO FUNCIONAMENTO DO MENU
-        function size(){
-            if(window.innerWidth >= 768){
-                itens.style.display = 'block'
-                icone.innerHTML = 'close'
-            }
-            else{
-                itens.style.display = 'none'
-                icone.innerHTML = 'menu'
-            }
-        }
+//FUNÇÃO PARA AJUDAR NO FUNCIONAMENTO DO MENU
+function size(){
+    if(window.innerWidth >= 768){
+        itens.style.display = 'block'
+        icone.innerHTML = 'close'
+    }
+    else{
+        itens.style.display = 'none'
+        icone.innerHTML = 'menu'
+    }
+}
 
-        //FUNÇÃO PARA QUANDO CLICAR NO HAMBURGUIN APRESENTAR O MENU
-        function menu_openned(){
-            if(menu.style.display == 'block'){
-                menu.style.display = 'none'
-                icone.innerHTML = 'menu'
-            }
-            else{
-                menu.style.display = 'block'
-                icone.innerHTML = 'close'
-            }
-        }
+//FUNÇÃO PARA QUANDO CLICAR NO HAMBURGUIN APRESENTAR O MENU
+function menu_openned(){
+    if(menu.style.display == 'block'){
+        menu.style.display = 'none'
+        icone.innerHTML = 'menu'
+    }
+    else{
+        menu.style.display = 'block'
+        icone.innerHTML = 'close'
+    }
+}
